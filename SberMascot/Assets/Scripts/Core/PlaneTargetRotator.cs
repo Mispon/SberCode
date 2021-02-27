67650df120a80ceedb4134914481af1427019f11
Mispon <tomythesupa@gmail.com>
2021-02-27 20:42:41 +0300

    новый спич бабл

---
+++ SberMascot/Assets/Scripts/Core/PlaneTargetRotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Core {
+    public class PlaneTargetRotator : MonoBehaviour
+    {
+        private void Update()
+        {
+            Vector3 lookDir = Vector3.zero - transform.position;
+            lookDir.x = 0;
+            lookDir.z = 0;
+            transform.rotation = Quaternion.LookRotation(lookDir);
+        }
+    }
+}
